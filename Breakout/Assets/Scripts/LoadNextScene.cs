0677234d16078ebdc1bc22bc427febb7167a1b60
Bhishman Desai <58675736+desaibhishman@users.noreply.github.com>
2020-02-22 21:56:22 +0530

    First Commit
    This is the first commit

---
+++ Breakout/Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene1()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        Scene cs = SceneManager.GetActiveScene();
+        string sceneName = cs.name;
+
+        if (sceneName == "End Game")
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+            SceneManager.LoadScene(currentScene + 1);
+    }
+
+    public void closeGame()
+    {
+        Application.Quit();
+    }
+}
